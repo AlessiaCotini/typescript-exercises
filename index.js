64fc328b240b52fdaad1667d227e567e919a7e7a
AlessiaCotini <alessia.cotini@icloud.com>
2026-05-11 10:57:44 +0200

    funzioni

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 "use strict";
 console.log("Hello typescript!");
+let x = 100;
+let firstName = "Stefano";
+let z = "Roberto";
+//assegnazione automatica del TIPO -> PROCESSO IMPLICITO
+// Z da questo momento è di tipo stringa
+// TYPE INFERENCE
+let boh = 50;
+boh = 100;
+boh = "Alessia";
+console.log(firstName.slice(0, 1).toUpperCase());
+const diceCiao = function () {
+    return "Ciao";
+};
+diceCiao();
+const lanciaDado = function () {
+    const lancio = Math.ceil(Math.random() * 6);
+    return lancio;
+};
+const n = lanciaDado();
+const funzioneSomma = function (n1, n2) {
+    return n1 + n2;
+};
+funzioneSomma(3, 4);
+const sommaDefault = function (n1, n2) {
+    return n1 + (n2 || 0);
+};
+console.log(sommaDefault(50));
--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
 console.log("Hello typescript!");
+
+let x: number = 100;
+let firstName: string = "Stefano";
+let z = "Roberto";
+//assegnazione automatica del TIPO -> PROCESSO IMPLICITO
+// Z da questo momento è di tipo stringa
+// TYPE INFERENCE
+let boh: any = 50;
+boh = 100;
+boh = "Alessia";
+
+console.log(firstName.slice(0, 1).toUpperCase());
+
+const diceCiao = function () {
+  return "Ciao";
+};
+diceCiao();
+
+const lanciaDado = function () {
+  const lancio = Math.ceil(Math.random() * 6);
+  return lancio;
+};
+const n = lanciaDado();
+
+const funzioneSomma = function (n1: number, n2: number) {
+  return n1 + n2;
+};
+funzioneSomma(3, 4);
+
+const sommaDefault = function (n1: number, n2?: number) {
+  return n1 + (n2 || 0);
+};
+
+console.log(sommaDefault(50));
